fec18eed6b46b27532692361d72841d53aee7658
XXT <ku8834367@163.com>
2018-08-27 23:54:50 +0800

    添加免认证的get接口

---
+++ application/api/controller/v1/GetData.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace app\api\controller\v1;
+
+use app\api\controller\Send;
+use app\api\model\Car;
+use app\api\model\CarData;
+use app\api\model\CarImage;
+use think\Controller;
+
+class GetData extends Controller
+{
+    use Send;
+
+    protected $query_get;
+
+    protected $result_get;
+
+    /**
+     * 处理简单的url参数
+     */
+    protected function baseParam()
+    {
+        $input = $_GET;
+        $result = [
+            'total' => 0,
+            'page' => 1,
+            'page_size' => 10,
+            'list' => []
+        ];
+        $query = [];
+        if (isset($input['uid'])) {
+            $query += ['uid' => $input['uid']];
+        }
+        if (isset($input['gid'])) {
+            $query += ['gid' => $input['gid']];
+        }
+        if (isset($input['g_sid'])) {
+            $query +=['g_sid' => $input['g_sid']];
+        }
+        if (isset($input['page'])) {
+            $result['page'] = (integer)$input['page'];
+        }
+        if (isset($input['limit'])) {
+            $result['page_size'] = (integer)$input['limit'];
+        }
+        $this->query_get = $query;
+        $this->result_get = $result;
+    }
+
+    /**
+     * @route('v1/notoken/car/data', 'get')
+     */
+    public function getData()
+    {
+        $this->baseParam();
+        $result = $this->result_get;
+        $tmp = CarData::where($this->query_get);
+        $tmp_count = clone $tmp;
+        $result['total'] = $tmp_count->count();
+        $result['list'] = $tmp->limit($result['page_size'])->page($result['page'])->select();
+        self::returnMsg(200, 'Get Data Successfully.', $result);
+    }
+
+    /**
+     * @route('/v1/notoken/car/image','get')
+     */
+    public function getImage()
+    {
+        $this->baseParam();
+        $result = $this->result_get;
+        $tmp = CarImage::where($this->query_get);
+        $tmp_count = clone $tmp;
+        $result['total'] = $tmp_count->count();
+        $result['list'] = $tmp->limit($result['page_size'])->page($result['page'])->select();
+        self::returnMsg(200, 'Get Data Successfully.', $result);
+    }
+
+    /**
+     * @route('/v1/notoken/car/user', 'get')
+     */
+    public function getUser()
+    {
+        $input = $_GET;
+        $result = [
+            'total' => 0,
+            'page' => 1,
+            'page_size' => 10,
+            'list' => []
+        ];
+        $query = [];
+        if (isset($input['uid'])) {
+            $query += ['uid' => $input['uid']];
+        }
+        if (isset($input['page'])) {
+            $result['page'] = (integer)$input['page'];
+        }
+        if (isset($input['limit'])) {
+            $result['page_size'] = (integer)$input['limit'];
+        }
+        $this->query_get = $query;
+        $this->result_get = $result;
+        $result = $this->result_get;
+        $tmp = Car::where($this->query_get);
+        $tmp_count = clone $tmp;
+        $result['total'] = $tmp_count->count();
+        $result['list'] = $tmp->limit($result['page_size'])->page($result['page'])->select();
+        self::returnMsg(200, 'Get Data Successfully.', $result);
+    }
+}
--- route/route.php
+++ route/route.php
@@ -20,5 +20,6 @@ Route::resource(':version/user','api/:version.user');       //资源路由
 Route::resource(':version/car/data', 'api/:version.CarDataRest');
 Route::resource(':version/car/image', 'api/:version.CarImageRest');
 
+Route::controller(':version/notoken/car', 'api/:version.GetData');
 //所有路由匹配不到情况下触发该路由
 Route::miss('\app\api\controller\Exception::miss');
